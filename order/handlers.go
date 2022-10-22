648039b97f952bfa0dc68741ae9791dfe7aaf497
scholak <scholaksebahattin@gmail.com>
2022-10-22 22:06:15 +0300

    http status code changed from not found to bad request in order handler tamamlandi function

--- order/handlers.go
+++ order/handlers.go
@@ -61,7 +61,7 @@ func Tamamlandi(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Sipariş tamamlandı."))
 	} else {
-		w.WriteHeader(http.StatusNotFound)
+		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("Sipariş kodu bulunamadı!"))
 	}
 }
